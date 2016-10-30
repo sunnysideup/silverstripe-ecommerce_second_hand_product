6735808b3e32a7d9811a2dc05b288b356962afd1
Nicolaas <github@sunnysideup.co.nz>
2016-10-30 15:32:35 +1300

    MINOR: adding task

--- code/tasks/EcommerceTaskSecondHandPublishAll.php
+++ code/tasks/EcommerceTaskSecondHandPublishAll.php
@@ -8,6 +8,7 @@ class EcommerceTaskSecondHandPublishAll extends BuildTask
 
     public function run($request)
     {
+        increase_time_limit_to(600);
         $products = SecondHandProduct::get()->filter(array('AllowPurchase' => 1));
         foreach($products as $product) {
             DB::alteration_message('Publish: '.$product->Title);
