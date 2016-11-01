937d0fc200e33fef877f4f385d7c0533bc261270
Sunny Side Up <github@sunnysideup.co.nz>
2016-11-01 14:21:35 +1300

    MINOR: adding ListOfFilters to Second Hand Product Group

--- code/SecondHandProductGroup.php
+++ code/SecondHandProductGroup.php
@@ -8,7 +8,7 @@ class SecondHandProductGroup extends ProductGroup
         'SecondHandProductGroup',
         'SecondHandProduct'
     );
-    
+
     private static $icon = 'ecommerce_second_hand_product/images/treeicons/SecondHandProductGroup';
 
     /**
@@ -35,6 +35,7 @@ class SecondHandProductGroup extends ProductGroup
      * @var string
      */
     private static $description = 'A product category page specifically for second had products';
+
 
 }
 
@@ -85,6 +86,27 @@ class SecondHandProductGroup_Controller extends ProductGroup_Controller
     {
         $term = Convert::raw2sql($request->param('searchterm'));
 
+    }
+
+    /**
+     * @return DataList 
+     */
+    function ListOfFilters()
+    {
+        $productIDs = $this->getProductsThatCanBePurchasedArray();
+        $sql = '
+            SELECT ProductGroupID
+            FROM Product_ProductGroups
+            WHERE ProductID IN ('.implode(',', $productIDs).')
+            GROUP BY ProductGroupID;
+        ';
+        $rows = DB::query($sql);
+        $idArray = array(0 => 0);
+        foreach($rows as $row) {
+            $idArray[$row['ProductGroupID']] = $row['ProductGroupID'];
+        }
+
+        return ProductGroup::get()->filter(array('ID' => $idArray));
     }
 
     function HasSearchFilterAndSort()
