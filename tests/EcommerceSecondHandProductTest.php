ab0bed25133a20ca2ade262f05d1375b686e5a6d
sunnysideup <sunnysideupdevs@gmail.com>
2017-01-03 14:10:01 +1300

    PATCH: module clean-up

---
+++ tests/EcommerceSecondHandProductTest.php
@@ -0,0 +1,13 @@
+<?php
+class EcommerceSecondHandProductTest extends SapphireTest {
+
+	protected $usesDatabase = false;
+
+	protected $requiredExtensions = array();
+
+	public function testMyMethod() {
+		$this->assertEquals(1, 1);
+	}
+
+}
+
