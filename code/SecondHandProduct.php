8ae161395194b14bbca7869d494193c370460a8d
Nicolaas Thiemen <nt@sunnysideup.co.nz>
2020-06-25 14:26:27 +1200

    MAJOR: upgrade to new version of Silverstripe - step: Add the table name

--- code/SecondHandProduct.php
+++ code/SecondHandProduct.php
@@ -22,6 +22,20 @@ class SecondHandProduct extends Product implements PermissionProvider
      * stadard SS declaration
      * @var Array
      */
+
+/**
+  * ### @@@@ START REPLACEMENT @@@@ ###
+  * OLD: private static $db (case sensitive)
+  * NEW: 
+    private static $table_name = '[SEARCH_REPLACE_CLASS_NAME_GOES_HERE]';
+
+    private static $db (COMPLEX)
+  * EXP: Check that is class indeed extends DataObject and that it is not a data-extension!
+  * ### @@@@ STOP REPLACEMENT @@@@ ###
+  */
+    
+    private static $table_name = 'SecondHandProduct';
+
     private static $db = array(
         'SoldPrice' => 'Currency',
         'PurchasePrice' => 'Currency',
--- code/SecondHandProductGroup.php
+++ code/SecondHandProductGroup.php
@@ -3,6 +3,20 @@
 
 class SecondHandProductGroup extends ProductGroup
 {
+
+/**
+  * ### @@@@ START REPLACEMENT @@@@ ###
+  * OLD: private static $db (case sensitive)
+  * NEW: 
+    private static $table_name = '[SEARCH_REPLACE_CLASS_NAME_GOES_HERE]';
+
+    private static $db (COMPLEX)
+  * EXP: Check that is class indeed extends DataObject and that it is not a data-extension!
+  * ### @@@@ STOP REPLACEMENT @@@@ ###
+  */
+    
+    private static $table_name = 'SecondHandProductGroup';
+
     private static $db = array(
         'RootParent' => 'Boolean'
     );
