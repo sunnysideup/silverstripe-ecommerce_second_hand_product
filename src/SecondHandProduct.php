3fab8c2f90b185fede80cd56c1e36fc7918301de
nicolaas <phoenix@sunnysideup.co.nz>
2022-02-27 15:59:16 +1300

    WIP

--- src/SecondHandProduct.php
+++ src/SecondHandProduct.php
@@ -208,19 +208,19 @@ class SecondHandProduct extends Product implements PermissionProvider, Permissio
     /**
      * Standard SS variable.
      */
-    private static $singular_name = 'SecondHand Product';
+    private static $singular_name = 'Second-Hand Product';
 
     /**
      * Standard SS variable.
      */
-    private static $plural_name = 'SecondHand Products';
+    private static $plural_name = 'Second-Hand Products';
 
     /**
      * standard SS declaration.
      *
      * @var string
      */
-    private static $description = 'This page displays a single secondhand product that can only be sold once';
+    private static $description = 'This page displays a single second-hand product that can only be sold once';
 
     public function SummaryFields()
     {
@@ -565,7 +565,7 @@ class SecondHandProduct extends Product implements PermissionProvider, Permissio
             $fields->removeByName('SellersAddressGeocodingField');
         }
         $fields->addFieldToTab(
-            'Root.Categorisation',
+            'Root.Under',
             $categoriesTable = $this->getProductGroupsTableField()
         );
 
@@ -640,6 +640,10 @@ class SecondHandProduct extends Product implements PermissionProvider, Permissio
         return $fields;
     }
 
+    /**
+     * products that can be sold SQL
+     * @return string
+     */
     public static function get_treshold_sql(): string
     {
         if ('' === self::$treshold_sql_cache) {
@@ -696,42 +700,22 @@ class SecondHandProduct extends Product implements PermissionProvider, Permissio
             ->byId($this->ID) ? true : false;
     }
 
+    /**
+     * this is the same as the
+     * @return bool [description]
+     */
     public function isUnderEmbargo(): bool
     {
-        $embargoDays = Config::inst()->get(SecondHandProduct::class, 'embargo_number_of_days');
-        if (intval($embargoDays) > 0) {
-            if ($this->DateItemWasBought) {
-                $date = $this->DateItemWasBought;
-            } else {
-                $date = $this->Created;
-            }
-            $createdDate = strtotime($date);
-            $daysOld = (time() - $createdDate) / (60 * 60 * 24);
-            if ($daysOld <= $embargoDays) {
-                return true;
-            }
-        }
-
-        return false;
+        return SecondHandProduct::get()
+            ->where(self::get_treshold_sql())
+            ->byId($this->ID) ? true : false;
     }
 
     public function didNotSell(): bool
     {
-        $embargoDays = Config::inst()->get(SecondHandProduct::class, 'embargo_number_of_days');
-        if (intval($embargoDays) > 0) {
-            if ($this->DateItemWasBought) {
-                $date = $this->DateItemWasBought;
-            } else {
-                $date = $this->Created;
-            }
-            $createdDate = strtotime($date);
-            $daysOld = (time() - $createdDate) / (60 * 60 * 24);
-            if ($daysOld <= $embargoDays) {
-                return true;
-            }
-        }
-
-        return false;
+        $shouldBeListedAfterTs = strtotime('-' . $daysMax . ' days', DBDatetime::now()->getTimestamp())
+        $listedTs = strtotime($this->Created);
+        return $listedTs < $shouldBeListedAfterTs;
     }
 
     public function HasBeenSold(): bool
