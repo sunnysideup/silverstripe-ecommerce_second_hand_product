126c93d35e3b607f3d35c54f94f210d4614e04d1
Nicolaas <github@sunnysideup.co.nz>
2025-08-19 18:27:27 +1200

    FIX: only run SH disable on LIVE

--- src/Api/SecondHandProductActions.php
+++ src/Api/SecondHandProductActions.php
@@ -4,6 +4,7 @@ namespace Sunnysideup\EcommerceSecondHandProduct\Api;
 
 use Exception;
 use SilverStripe\CMS\Model\SiteTree;
+use SilverStripe\Control\Director;
 use SilverStripe\Control\HTTPResponse;
 use SilverStripe\ORM\DB;
 use SilverStripe\Versioned\Versioned;
@@ -24,52 +25,54 @@ class SecondHandProductActions
 
     public static function quick_disable_or_enable(bool $enable, $buyable, ?int $archivedByID = 0, ?string $databaseName = '')
     {
-        if ($buyable && $buyable->exists() && $buyable->InternalItemID) {
-            $dbName = $databaseName ? $databaseName : DB::get_conn()->getSelectedDatabase();
+        if (Director::isLive()) {
+            if ($buyable && $buyable->exists() && $buyable->InternalItemID) {
+                $dbName = $databaseName ? $databaseName : DB::get_conn()->getSelectedDatabase();
 
-            foreach (['', '_Live'] as $ext) {
-                $archivedByLine = '';
-                $dateItemSoldLine = '';
-                $priceLine = '';
-                $zeroOrOne = 0;
-                if(!$archivedByID) {
-                    $archivedByID = 0;
-                }
-                if (false === $enable) {
-                    if ($archivedByID) {
-                        $archivedByLine = 'SecondHandProduct' . $ext . '.ArchivedByID = ' . $archivedByID . ',';
-                    }
-                    $dateItemSoldLine = '' . $dbName . '."SecondHandProduct' . $ext . '"."DateItemWasSold" = \'' . date('Y-m-d') . '\',';
+                foreach (['', '_Live'] as $ext) {
+                    $archivedByLine = '';
+                    $dateItemSoldLine = '';
+                    $priceLine = '';
                     $zeroOrOne = 0;
-                } else {
-                    $price = $buyable->Price ?: 0;
-                    $priceLine = '' . $dbName . '.Product' . $ext . '.Price = ' . $price . ',';
-                    $archivedByLine = '' . $dbName . '.SecondHandProduct' . $ext . '.ArchivedByID = 0,';
-                    $dateItemSoldLine = '' . $dbName . '."SecondHandProduct' . $ext . '"."DateItemWasSold" = \'\',';
-                    $zeroOrOne = 1;
+                    if (!$archivedByID) {
+                        $archivedByID = 0;
+                    }
+                    if (false === $enable) {
+                        if ($archivedByID) {
+                            $archivedByLine = 'SecondHandProduct' . $ext . '.ArchivedByID = ' . $archivedByID . ',';
+                        }
+                        $dateItemSoldLine = '' . $dbName . '."SecondHandProduct' . $ext . '"."DateItemWasSold" = \'' . date('Y-m-d') . '\',';
+                        $zeroOrOne = 0;
+                    } else {
+                        $price = $buyable->Price ?: 0;
+                        $priceLine = '' . $dbName . '.Product' . $ext . '.Price = ' . $price . ',';
+                        $archivedByLine = '' . $dbName . '.SecondHandProduct' . $ext . '.ArchivedByID = 0,';
+                        $dateItemSoldLine = '' . $dbName . '."SecondHandProduct' . $ext . '"."DateItemWasSold" = \'\',';
+                        $zeroOrOne = 1;
+                    }
+                    $sql = '
+                        UPDATE ' . $dbName . '.Product' . $ext . '
+                            INNER JOIN ' . $dbName . '.SiteTree' . $ext . '
+                                ON ' . $dbName . '.SiteTree' . $ext . '.ID = ' . $dbName . '.Product' . $ext . '.ID
+                            INNER JOIN ' . $dbName . '.SecondHandProduct' . $ext . '
+                                ON ' . $dbName . '.SecondHandProduct' . $ext . '.ID = ' . $dbName . '.Product' . $ext . '.ID
+
+                        SET
+                            ' . $priceLine . '
+                            ' . $dateItemSoldLine . '
+                            ' . $archivedByLine . '
+                            ' . $dbName . '."Product' . $ext . '"."FeaturedProduct" = ' . ($zeroOrOne) . ',
+                            ' . $dbName . '."Product' . $ext . '"."AllowPurchase" = ' . $zeroOrOne . ',
+                            ' . $dbName . '."SiteTree' . $ext . '"."ShowInMenus" = ' . $zeroOrOne . ',
+                            ' . $dbName . '."SiteTree' . $ext . '"."ShowInSearch" = ' . $zeroOrOne . '
+
+                        WHERE
+                            ' . $dbName . '."Product' . $ext . '"."InternalItemID" = \'' . $buyable->InternalItemID . '\'
+                        ;
+                    ';
+                    // limit can not be used here...
+                    DB::query($sql);
                 }
-                $sql = '
-                    UPDATE ' . $dbName . '.Product' . $ext . '
-                        INNER JOIN ' . $dbName . '.SiteTree' . $ext . '
-                            ON ' . $dbName . '.SiteTree' . $ext . '.ID = ' . $dbName . '.Product' . $ext . '.ID
-                        INNER JOIN ' . $dbName . '.SecondHandProduct' . $ext . '
-                            ON ' . $dbName . '.SecondHandProduct' . $ext . '.ID = ' . $dbName . '.Product' . $ext . '.ID
-
-                    SET
-                        ' . $priceLine . '
-                        ' . $dateItemSoldLine . '
-                        ' . $archivedByLine . '
-                        ' . $dbName . '."Product' . $ext . '"."FeaturedProduct" = ' . ($zeroOrOne) . ',
-                        ' . $dbName . '."Product' . $ext . '"."AllowPurchase" = ' . $zeroOrOne . ',
-                        ' . $dbName . '."SiteTree' . $ext . '"."ShowInMenus" = ' . $zeroOrOne . ',
-                        ' . $dbName . '."SiteTree' . $ext . '"."ShowInSearch" = ' . $zeroOrOne . '
-
-                    WHERE
-                        ' . $dbName . '."Product' . $ext . '"."InternalItemID" = \'' . $buyable->InternalItemID . '\'
-                    ;
-                ';
-                // limit can not be used here...
-                DB::query($sql);
             }
         }
     }
